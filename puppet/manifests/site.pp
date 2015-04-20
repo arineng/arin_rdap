df4aac6576ccbd2d286b47a4dfb05dd55d066fc1
Jacob <jacobcastello@gmail.com>
2015-04-20 12:30:55 -0400

    better logic for the manifests

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,24 +1,21 @@
-stage {'pre':
-  before => Stage['main']
-}
+# Baseconfig includes necessary packages for build
+include baseconfig
 
-class { 'baseconfig':
-  stage => 'pre'
-}
-
-vcsrepo { '/home/vagrant/code':
+# Be sure to clone after package installs
+# Be sure to run the maven build after cloning
+vcsrepo { '/opt/rdap/code':
   ensure   => present,
   provider => git,
   source   => "http://stash-projects.arin.net:7990/scm/rrr/code.git",
   user     => 'vagrant',
   owner    => 'vagrant',
   group    => 'vagrant',
+  require  => [ Class["baseconfig"]]
   before   => Exec['mvn_package'],
 }
 
+# Run the package build command
 exec { 'mvn_package':
   command => "/usr/bin/mvn package -e -f /home/vagrant/code/pom.xml",
-  user    => 'vagrant',
 }
 
-include baseconfig
