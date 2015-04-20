b9203b9daf6298f2ccccc5aaa139b633a2c5e5f8
Jacob <jacobcastello@gmail.com>
2015-04-20 15:22:39 -0400

    using class

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -3,19 +3,21 @@ include baseconfig
 
 # Be sure to clone after package installs
 # Be sure to run the maven build after cloning
+
+file { '/opt/rdap':
+  ensure => directory
+}
+
 vcsrepo { '/opt/rdap/code':
   ensure   => present,
   provider => git,
   source   => "http://stash-projects.arin.net:7990/scm/rrr/code.git",
-  user     => 'vagrant',
-  owner    => 'vagrant',
-  group    => 'vagrant',
-  require  => [ Class["baseconfig"]]
+  require  => [ Class["baseconfig"]],
   before   => Exec['mvn_package'],
 }
 
 # Run the package build command
 exec { 'mvn_package':
-  command => "/usr/bin/mvn package -e -f /home/vagrant/code/pom.xml",
+  command => "/usr/bin/mvn package -e -f /opt/rdap/code/pom.xml",
 }
 
