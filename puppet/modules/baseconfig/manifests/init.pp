8153e6724db732591aa0f26737f142aeae69c22a
Jacob <jacobcastello@gmail.com>
2015-04-20 15:23:10 -0400

    centos cases

--- puppet/modules/baseconfig/manifests/init.pp
+++ puppet/modules/baseconfig/manifests/init.pp
@@ -3,20 +3,29 @@
 # Performs initial configuration tasks for all Vagrant boxes.
 #
 class baseconfig {
-  exec { 'apt-get update':
-    command => '/usr/bin/apt-get update';
+  case $operatingsystem  {
+    CentOS: { 
+      $update_command = '/usr/bin/yum update -y' 
+      $java_packages = ['java-1.6.0-openjdk', 'apache-maven', 'git']
+      yumrepo { "apache-maven":
+        baseurl  => 'http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-$releasever/$basearch/',
+        descr    => "Apache Maven from fedorapeople",
+        enabled  => 1,
+        gpgcheck => 0,
+        before   => Package[$java_packages]
+      }
+    } 
+    Ubuntu: { 
+      $update_command = '/usr/bin/apt-get update'
+      $java_packages = ['maven', 'maven2', 'default-jdk']
+    } 
+  }
+  exec { $update_command :
+    command => $update_command,
   }
 
-  Package {
+  package { $java_packages : 
     ensure  => present,
-    require => Exec['apt-get update'],
+    require => Exec[$update_command],
   }
-
-  package { 'git': }
-
-  package { 'maven': }
-
-  package { 'maven2':}
-
-  package { 'default-jdk': }
 }
