805e6b0dda51c4cc50e94b0c8e2f773e2aa0b162
Jacob <jacobcastello@gmail.com>
2015-04-21 17:01:54 -0400

    added tomcat deploy and nicinfo

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,5 +1,6 @@
 # Baseconfig includes necessary packages for build
 include baseconfig
+include stdlib
 
 # Be sure to clone after package installs
 # Be sure to run the maven build after cloning
@@ -16,8 +17,26 @@ vcsrepo { '/opt/rdap/code':
   before   => Exec['mvn_package'],
 }
 
+vcsrepo { '/opt/nicinfo':
+  ensure   => present,
+  provider => git,
+  source   => "http://stash-projects.arin.net:7990/scm/NIC/main.git",
+}
+
 # Run the package build command
 exec { 'mvn_package':
   command => "/usr/bin/mvn package -e -f /opt/rdap/code/pom.xml",
 }
 
+class { 'tomcat': }
+tomcat::service { 'default':
+  use_jsvc     => false,
+  use_init     => true,
+  service_name => 'tomcat7',
+}
+
+tomcat::war { 'rdap_bootstrap_server-1000.0-SNAPSHOT.war':
+  catalina_base => '/var/lib/tomcat7',
+  war_source    => '/opt/rdap/code/target/rdap_bootstrap_server-1000.0-SNAPSHOT.war',
+  require       => Exec['mvn_package'],
+}
