a9ab88736d48996259b061c42472a1536c8ed61f
Jacob <jacobcastello@gmail.com>
2015-04-22 12:36:48 -0400

    adding epel and tomcat package

--- puppet/modules/baseconfig/manifests/init.pp
+++ puppet/modules/baseconfig/manifests/init.pp
@@ -6,7 +6,7 @@ class baseconfig {
   case $operatingsystem  {
     CentOS: { 
       $update_command = '/usr/bin/yum update -y' 
-      $java_packages = ['java-1.6.0-openjdk', 'apache-maven', 'git']
+      $java_packages = ['java-1.6.0-openjdk', 'apache-maven', 'git', 'tomcat6']
       yumrepo { "apache-maven":
         baseurl  => 'http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-$releasever/$basearch/',
         descr    => "Apache Maven from fedorapeople",
@@ -14,10 +14,17 @@ class baseconfig {
         gpgcheck => 0,
         before   => Package[$java_packages]
       }
+      yumrepo { "epel":
+        baseurl  => 'http://download.fedoraproject.org/pub/epel/6/$basearch',
+        descr    => 'Extra Packages for Enterprise Linux 6 - $basearch',
+        enabled  => 1,
+        gpgcheck => 0,
+        before   => Package[$java_packages]
+      }
     } 
     Ubuntu: { 
       $update_command = '/usr/bin/apt-get update'
-      $java_packages = ['maven', 'maven2', 'default-jdk', 'git']
+      $java_packages = ['maven', 'maven2', 'default-jdk', 'git', 'tomcat6']
     } 
   }
   exec { $update_command :
